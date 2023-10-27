711e1963199532eef51ff910d7d9317aebe7b829
clemleri <lerouley.2005@gmail.com>
2023-10-27 12:20:26 +0200

    doublon4

--- recherche/doublons4/doublons4.go
+++ recherche/doublons4/doublons4.go
@@ -1,5 +1,7 @@
 package doublons4
 
+import "fmt"
+
 /*
 On dispose d'un tableau d'entiers de longueur n et on suppose qu'il contient
 exactement k fois chaque nombre compris entre 1 et n/k. On voudrait vérifier
@@ -16,5 +18,28 @@ entiers de 1 à len(tab)/k
 */
 
 func doublons(tab []int) (k int, ok bool) {
-	return k, ok
+	if len(tab) == 0{
+		return k, true
+	}
+	for y:=0; y<len(tab);y++ {
+		if tab[0]==tab[y] {
+			k++
+		}
+	}
+	fmt.Println(k)
+	for i:=0;i<len(tab)/k;i++ {
+		cpt:=0
+		for j:=0; j<len(tab); j++{
+			if i==tab[j] {
+				fmt.Println("i est bien égale tab[j]")
+				cpt++
+			}
+		}
+		if cpt!=k{
+			fmt.Println("cpt: ",cpt)
+			return k, false
+		}
+	}
+	fmt.Println("k return: ",k )
+	return k, true
 }
