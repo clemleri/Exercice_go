d7fca4b2e3f88017b36f12e0b0c5319c66cb1ff6
clemleri <lerouley.2005@gmail.com>
2023-11-28 08:11:29 +0100

    fonction contient

--- structures/contient/contient.go
+++ structures/contient/contient.go
@@ -1,9 +1,12 @@
 package contient
 
 /*
-On dispose d'une structure de liste chaînée contenent des éléments avec chacun une valeur. On souhaite, étant donnée une liste, savoir si elle contient une valeur particulière (savoir si un de ses éléments a cette valeur) grâce à la méthode contient. Cette méthode ne doit pas modifier la liste.
+On dispose d'une structure de liste chaînée contenent des éléments avec chacun une valeur.
+On souhaite, étant donnée une liste, savoir si elle contient une valeur particulière (savoir si un de ses 
+éléments a cette valeur) grâce à la méthode contient. Cette méthode ne doit pas modifier la liste.
 
-On considère qu'il n'y a jamais de cycles dans nos listes : en allant de suivant en suivant on arrive obligatoirement au bout d'un moment sur un dernier élément (dont le suivant est nil).
+On considère qu'il n'y a jamais de cycles dans nos listes : en allant de suivant en suivant on arrive obligatoirement 
+au bout d'un moment sur un dernier élément (dont le suivant est nil).
 
 Pour cet exercice les boucles for sont interdites.
 
@@ -28,5 +31,11 @@ type element struct {
 }
 
 func (l liste) contient(v int) (existe bool) {
-	return
+	if l.debut.valeur == v{
+		return true
+	}else if l.debut.suivant == nil{
+		return false
+	}else {
+		return l.debut.suivant.contient(v)
+	}
 }
